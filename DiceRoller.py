cfadb408f581aa7b667e637c341d58073a4bfe36
pushnotif <55043477+pushnotif@users.noreply.github.com>
2019-09-11 17:45:53 +0800

    Update DiceRoller.py

--- DiceRoller.py
+++ DiceRoller.py
@@ -7,15 +7,18 @@ while  try_again == True:
 
     # prompt user for number of dice rolls
     num_rolls = int(input("Enter number of dice rolls: "))
+    num_side = int(input("Enter number of sides: "))
 
-    # variables for dice value and count for loop statement
-    dice_value = [1, 2, 3, 4, 5, 6]
+    # variable for count variable for loop statement
     count = 0
 
     # loop statement for number of rolls results
 
     while count < num_rolls:
-            print(random.choice(dice_value))
+            # variables for dice value, "num_side+1" since last item isn't considered
+            # i.e for 10 sided dice, 10 wont be picked as a result if rolled
+            dice_value = random.randrange(1, num_side + 1, 1)
+            print("You roll: ",dice_value)
             count = count + 1
 
     # prompt user to play again
